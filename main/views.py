e496bb034368c5e608fb7e3826d48deda93cbac3
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-04-15 14:01:08 +0500

    set up home view to handle post request

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views.generic import View
 from .languages import lang_list
@@ -6,4 +7,15 @@ from .languages import lang_list
 class HomePageView(View):
     def get(self, request, *args, **kwargs):
         context = {"lang_list": lang_list}
+        return render(request, "main/index.html", context)
+
+    def post(self, request, *args, **kwargs):
+        code = request.POST.get("code")
+        lang = request.POST.get("lang")
+
+        if lang == "Select Programming Language":
+            messages.warning(request, "You forgot to select a language")
+
+        context = {"code": code, "lang_list": lang_list}
+
         return render(request, "main/index.html", context)
