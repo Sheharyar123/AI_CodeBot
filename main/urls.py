ebba102225c492b01b6c9a7dc93081198f1cfc43
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-04-17 07:35:22 +0500

    create functionality to let openai api suggest code for a given prompt

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, SuggestView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='index'),
+    path("", HomePageView.as_view(), name="index"),
+    path("suggest/", SuggestView.as_view(), name="suggest"),
 ]
--- main/views.py
+++ main/views.py
@@ -29,7 +29,7 @@ class HomePageView(View):
                     model="text-davinci-003",
                     prompt=f"Respond only with code. Fix this {lang.lower()} code: { code}",
                     temperature=0,
-                    max_tokens=1000,
+                    max_tokens=500,
                     top_p=1,
                     frequency_penalty=0.0,
                     presence_penalty=0.0,
@@ -42,3 +42,41 @@ class HomePageView(View):
         context = {"code": code, "lang": lang, "lang_list": lang_list}
 
         return render(request, "main/index.html", context)
+
+
+class SuggestView(View):
+    def get(self, request, *args, **kwargs):
+        context = {"lang_list": lang_list}
+        return render(request, "main/suggest.html", context)
+
+    def post(self, request, *args, **kwargs):
+        code = request.POST.get("code")
+        lang = request.POST.get("lang")
+
+        if lang == "Select Programming Language":
+            print(lang)
+            messages.warning(request, "You forgot to select a language")
+
+        else:
+            openai.api_key = settings.API_KEY
+            # # Create OpenAI Instance
+            # openai.Model.list()
+            try:
+                # Call the API
+                response = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt=f"Suggest code in {lang} for {code}. Just respond with code",
+                    temperature=0,
+                    max_tokens=1000,
+                    top_p=1,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0,
+                )
+                code = response["choices"][0]["text"].strip()
+                print(code)
+            except Exception as e:
+                print(e)
+
+        context = {"code": code, "lang": lang, "lang_list": lang_list}
+
+        return render(request, "main/suggest.html", context)
